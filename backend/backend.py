d21e7998a166d50ac4a6d62c4c6176e9c6709f78
LEGEND2310 <deepakkumarjha178531@gmail.com>
2021-03-14 21:44:15 +0530

    backend code

---
+++ backend/backend.py
@@ -0,0 +1,52 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import geocoder
+from datetime import datetime
+
+g = geocoder.ip('me')
+
+
+listner = sr.Recognizer()
+engine = pyttsx3.init()
+trigger = "help"
+stop = "stop"
+contacts = [
+    {
+        "name" : "Ishika",
+        "number" : "+917007070964" 
+    },
+    {
+        "name" : "Anu",
+        "number" : "+918005038609"
+    }
+]
+
+def send_text():
+    for contact in contacts:
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        hour,mint = int(current_time.split(":")[0]),int(current_time.split(":")[1])
+        pywhatkit.sendwhatmsg(contact['number'],str(g.latlng),hour,mint+2)
+
+def listen():
+
+    try:
+        with sr.Microphone() as source:
+            print("Listening...")
+            voice = listner.listen(source)
+            command = listner.recognize_google(voice)
+            command = command.lower()
+            print(command)
+            if trigger in command:
+                print("Creating SOS Alert!!!")
+                send_text()
+
+            elif stop in command:
+                raise ValueError("Terminating the application.")
+    
+    except sr.UnknownValueError:
+        pass
+
+while True:
+    listen()
